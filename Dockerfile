4ead0d859a1fab4dfaa77f67f715a257471de4f2
Liam Bindle <liambindle@gmail.com>
2019-09-20 15:27:26 -0500

    initial version

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+ARG BASE_IMAGE
+ARG BASE_IMAGE_TAG
+FROM ${BASE_IMAGE}:${BASE_IMAGE_TAG}
+
+MAINTAINER Liam Bindle <liam.bindle@gmail.com>
+
+ARG INSTALL_CONFIG
+COPY ${INSTALL_CONFIG} /
+RUN bash /compilers.sh
+RUN bash /utilities.sh
+ENV MODULEPATH=${MODULEPATH}:/modulefiles
+ENV SPACK_ROOT=/spack
+ENV PATH=${SPACK_ROOT}/bin:${PATH}
---
+++ centos/utilities.sh
@@ -0,0 +1,18 @@
+set -x
+set -e
+
+yum install -y bzip2 zip wget curl autoconf automake flex bison make python git vim which environment-modules 
+
+mkdir /modulefiles
+mkdir /modulefiles/gcc
+cd /modulefiles
+/usr/share/Modules/bin/createmodule.sh /opt/rh/devtoolset-7/enable > gcc/7
+/usr/share/Modules/bin/createmodule.sh /opt/rh/devtoolset-8/enable > gcc/8
+export MODULEPATH=$MODULEPATH:/modulefiles
+
+cd /
+git clone https://github.com/spack/spack.git
+cd spack
+git checkout v0.12.1
+. share/spack/setup-env.sh
+spack bootstrap
